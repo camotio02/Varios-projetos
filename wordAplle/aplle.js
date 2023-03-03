480d53add266fa457a52195120f0dca107ec7e21
Temotio Luis <temotio.bernardo@snowmanlabs.com>
2023-03-03 12:44:10 -0300

    first commit

--- wordAplle/aplle.js
+++ wordAplle/aplle.js
@@ -1,6 +1,3 @@
 const keyCode = document.querySelectorAll(`.key[data-key="18"]`)
 const key = document.querySelectorAll(`.key`)
 let input = document.querySelector('input#writerText');
-const doSomething =()=> {
-    input.addEventListener("keydown")
-}
