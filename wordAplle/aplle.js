617f088fb929560449705060c51d3be746df8da5
camotio02 <temotio.bernardo@snowmanlabs.com>
2023-04-06 16:20:29 -0300

    add novas funções do teclado

--- wordAplle/aplle.js
+++ wordAplle/aplle.js
@@ -1,3 +1,63 @@
-const keyCode = document.querySelectorAll(`.key[data-key="18"]`)
-const key = document.querySelectorAll(`.key`)
-let input = document.querySelector('input#writerText');
+// get all keys
+const keys = document.querySelectorAll(".key")
+const input = document.querySelector("input#writeText")
+const keysText = document.querySelectorAll(".key").values
+function playNote(event) {
+
+    let audioKeyCode = getKeyCode(event);
+
+    // typed or pressed key
+    const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+    addPlayingClass(key)
+}
+
+function addPlayingClass(key) {
+    const contents = [
+        "delete",
+        "enter",
+        "enter return",
+        "shift",
+        "tab",
+        "command",
+        "alt option",
+        "caps lock",
+        "control",
+        "center"
+    ]
+    if (contents.find((item) => item === key.textContent)) {
+        return key.classList.add('especial')
+    }
+    key.classList.add('playing')
+}
+function getKeyCode(event) {
+    let keyCode;
+    const isKeyboard = event.type === "keydown"
+
+    if (isKeyboard) {
+        keyCode = event.keyCode
+    } else {
+        keyCode = event.target.dataset.key
+    }
+
+    return keyCode
+}
+
+
+function removePlayingClass(event) {
+    event.target.classList.remove("playing")
+    event.target.classList.remove("especial")
+}
+
+
+function registerEvents() {
+    // click with mouse
+    keys.forEach(function (key) {
+        key.addEventListener("click", playNote)
+        key.addEventListener("transitionend", removePlayingClass)
+    })
+
+    // keyboard type
+    window.addEventListener("keydown", playNote)
+}
+
+window.addEventListener("load", registerEvents)
